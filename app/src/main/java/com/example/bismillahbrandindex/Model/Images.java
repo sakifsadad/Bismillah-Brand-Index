3ec165757cc3be835eb7860b69d5472650e057be
S M Sakif Sadad Dil <sakifsadad@gmail.com>
2019-04-15 00:29:29 +0600

    image array

---
+++ app/src/main/java/com/example/bismillahbrandindex/Model/Images.java
@@ -0,0 +1,19 @@
+package com.example.bismillahbrandindex.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Images {
+    private List<String> images = new ArrayList<>();
+
+    public Images() {
+
+    }
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+}
